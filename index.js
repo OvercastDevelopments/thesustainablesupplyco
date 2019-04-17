1f02377af139c89f78cf5db0f68f450c0f110744
Overcast Developments <49539921+OvercastDevelopments@users.noreply.github.com>
2019-04-17 13:16:12 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function aboutpage(){
     document.location.href = "about.html";
 }
+function snapchat() {
+    alert("Fill out our contact form to find out more");
+}
+function linkedin() {
+    window.open("https://nz.linkedin.com/in/leigh-crooke-15574530");
+}
+function instagram(){
+    window.open("https://www.instagram.com");
+}
+function facebook() {
+    window.open("https://www.facebook.com/overcastdevelopments");
+}
